48d52b58db0e200793c9a5837974ab0006d5618e
markred0430 <redilamarkanthony30@gmail.com>
2022-12-16 22:45:41 +0800

    Queues.py

--- Stacks.py
+++ Stacks.py
@@ -1,1 +1,13 @@
-print("Hello World.") #Test
+# Queues.py
+
+from collections import deque
+
+class Queue:
+    def __init__(self):
+        self._elements = deque()
+
+    def enqueue(self, element):
+        self._elements.append(element)
+
+    def dequeue(self):
+        return self._elements.popleft()
