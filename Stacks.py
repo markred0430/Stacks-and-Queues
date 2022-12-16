1424af3f7b383d24b3e397f5564eddcceb5d1c6d
markred0430 <redilamarkanthony30@gmail.com>
2022-12-16 22:53:36 +0800

    Queues.py (2)

--- Stacks.py
+++ Stacks.py
@@ -1,10 +1,30 @@
 # Queues.py
 
+#from collections import deque
+
+#class Queue:
+ #   def __init__(self):
+  #      self._elements = deque()
+
+  #  def enqueue(self, element):
+  #      self._elements.append(element)
+
+#    def dequeue(self):
+ #       return self._elements.popleft()
+
 from collections import deque
 
+
 class Queue:
-    def __init__(self):
-        self._elements = deque()
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
 
     def enqueue(self, element):
         self._elements.append(element)
