9f5f13f015b7557865b7417f26297aee9f3297fd
markred0430 <redilamarkanthony30@gmail.com>
2022-12-20 16:04:00 +0800

    Test 20

--- Stacks.py
+++ Stacks.py
@@ -12,22 +12,24 @@
 #    def dequeue(self):
  #       return self._elements.popleft()
 
-from collections import deque
+#from collections import deque
 
 
-class Queue:
-    def __init__(self, *elements):
-        self._elements = deque(elements)
+#class Queue:
+    #def __init__(self, *elements):
+        #self._elements = deque(elements)
 
-    def __len__(self):
-        return len(self._elements)
+    #def __len__(self):
+       # return len(self._elements)
 
-    def __iter__(self):
-        while len(self) > 0:
-            yield self.dequeue()
+    #def __iter__(self):
+       # while len(self) > 0:
+            #yield self.dequeue()
 
-    def enqueue(self, element):
-        self._elements.append(element)
+    #def enqueue(self, element):
+        #self._elements.append(element)
 
-    def dequeue(self):
-        return self._elements.popleft()
+    #def dequeue(self):
+        #return self._elements.popleft()
+
+print("Hello World") #Test
