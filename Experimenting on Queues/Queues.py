796818cc73dd5a9c0ce19a4264db35596e9ef522
markred0430 <redilamarkanthony30@gmail.com>
2022-12-21 16:26:24 +0800

    Improving the Queues content

--- Experimenting on Queues/Queues.py
+++ Experimenting on Queues/Queues.py
@@ -1,56 +1,11 @@
-# Queues.py
+from collections import deque
 
-#from collections import deque
+class Queue:
+    def __init__(self):
+        self._elements = deque()
 
-#class Queue:
- #   def __init__(self):
-  #      self._elements = deque()
+    def enqueue(self, element):
+        self._elements.append(element)
 
-  #  def enqueue(self, element):
-  #      self._elements.append(element)
-
-#    def dequeue(self):
- #       return self._elements.popleft()
-
-#from collections import deque
-
-
-#class Queue:
-    #def __init__(self, *elements):
-        #self._elements = deque(elements)
-
-    #def __len__(self):
-       # return len(self._elements)
-
-    #def __iter__(self):
-       # while len(self) > 0:
-            #yield self.dequeue()
-
-    #def enqueue(self, element):
-        #self._elements.append(element)
-
-    #def dequeue(self):
-        #return self._elements.popleft()
-
-# Implementing Queues
-#from heapq import heappush
-
-#fruits = []
-#heappush(fruits, "orange")
-#heappush(fruits, "apple")
-#heappush(fruits, "banana")
-
-#print(fruits)
-
-#from heapq import heappop
-#heappop(fruits)
-#print(fruits)
-
-# Tuple Comparison
-#student1 = ("Mark", "Redila", 19)
-#student2 = ("Anthony", "Red", 20)
-
-#print(student2>student1)
-
-
-
+    def dequeue(self):
+        return self._elements.popleft()
